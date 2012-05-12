df64fab28476eb5a95a7da61f98e8f51db916dcb
Robert <Robert@MININT-R11HSAL>
2012-05-12 02:06:09 -0700

    Made Internet proxy compatible with more ways to retrieve media sources.

--- proxy.php
+++ proxy.php
@@ -11,7 +11,12 @@ $file = $html->load_file($url);
 $sources = array();
 
 // Retrieve the parts we care about
-$iframes = $html->find('iframe');
+$links = $html->find("a");
+foreach($links as $link) {
+    $sources[] = $link->href;
+}
+
+$iframes = $html->find("iframe");
 foreach ($iframes as $iframe) {
     $sources[] = $iframe->src;
 }
@@ -19,6 +24,11 @@ foreach ($iframes as $iframe) {
 $embeds = $html->find("embed");
 foreach ($embeds as $embed) {
     $sources[] = $embed->src;
+}
+
+$objects = $html->find("object");
+foreach ($objects as $object) {
+    $sources[] = $object->data;
 }
 
 
