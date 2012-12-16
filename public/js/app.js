4a692dbaa4c4044386309d66f81f84779e26e2e4
Robert Fruchtman <rfruchtose@gmail.com>
2012-12-15 21:11:35 -0700

    Create app.js for storing app configuration and starting the Backbone components.

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+soundManager.debugMode = false;
+soundManager.flashVersion = 9;
+soundManager.preferFlash = false;
+soundManager.url = 'swf/';
+
+var YouTube = new YouTubeInterface({el: $("#video")});
+var Playlist = new TrackPlaylist();
+var SearchResults = new SearchResultsProvider();
+$(document).ready(function() {
+	var router = new PlaylistRouter();
+	var mainView = new MainView().toggleBlock().render();
+	Playlist.once('sync', function() {
+		mainView.toggleBlock();
+	});
+	Playlist.on('sync', function(data) {
+		var fragment = (data.id) ? data.id.toString() : "";
+		router.navigate(fragment, {trigger: false});
+	});
+	Backbone.history.start({pushState: true});
+});
