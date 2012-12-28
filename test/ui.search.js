8a9dd61aee1fb2447cc0d44abe7830443e067f04
Robert Fruchtman <rfruchtose@gmail.com>
2012-12-28 13:27:58 -0700

    Add test structure for more automated server testing.

--- test/ui.search.js
+++ test/ui.search.js
@@ -4,45 +4,39 @@ var Q 		  = require('q'),
 	muxamp 	  = require('../lib/server').getApplication();
 
 describe('Frontend search', function() {
-	var port = 3000, testEnv = this;
-	before(function(done) {
-		(testEnv.app = muxamp.listen(port)).on('listening', function() {
-			testEnv.browser = new Browser();
-			done();
+	var baseUrl = 'http://localhost:' + 3000 + '/', setup;
+	var browser, window;
+	setup = {
+		before: function() { browser = new Browser(); window = browser.window; },
+		after: function() { browser.close(); }
+	};
+	testutils.server.testWithServer(3000, function() {
+		it('should have the expected UI', function(done) {
+			testutils.expectSuccess(browser.visit(baseUrl), function() {
+				window.$('#search-query').size().should.eql(1);
+				window.$('#search-submit').size().should.eql(1);
+				window.$('#site-selector').size().should.eql(1);
+				window.$('#search-site-dropdown').find('li').size().should.eql(3); // 3 options
+			}, done);
 		});
-		testEnv.baseUrl = 'http://localhost:' + port + '/';
-	});
-	after(function(done) {
-		testEnv.app.on('close', done);
-		testEnv.app.close();
-	});
-
-	it('should have the expected UI', function(done) {
-		testutils.expectSuccess(testEnv.browser.visit(testEnv.baseUrl), function() {
-			var window = testEnv.browser.window;
-			window.$('#search-query').size().should.eql(1);
-			window.$('#search-submit').size().should.eql(1);
-			window.$('#site-selector').size().should.eql(1);
-			window.$('#search-site-dropdown').find('li').size().should.eql(3); // 3 options
-		}, done);
-	});
-	it('should be able to contact the server', function(done) {
-		var testCase = function() {
-			var deferred = Q.defer();
-			testEnv.browser.visit(testEnv.baseUrl).then(function() {
-			testEnv.browser
-				.fill('#search-query', 'rebecca black')
-				.pressButton('#search-submit', function() {
-					deferred.resolve(testEnv.browser);
+		it('should be able to contact the server', function(done) {
+			var testCase = function() {
+				var deferred = Q.defer();
+				browser.visit(baseUrl).then(function() {
+					browser
+						.fill('#search-query', 'rebecca black')
+						.pressButton('#search-submit', function() {
+							deferred.resolve(browser);
+						});
+					
+				}, function(error) {
+					throw error;
 				});
-			
-			}, function(error) {
-				throw error;
-			});
-			return deferred.promise;
-		};
-		testutils.expectSuccess(testCase(), function(browser) {
-			browser.window.$('#search-results tbody tr').size().should.eql(25);
-		}, done);
-	});
+				return deferred.promise;
+			};
+			testutils.expectSuccess(testCase(), function(browser) {
+				window.$('#search-results tbody tr').size().should.eql(25);
+			}, done);
+		});
+	}, setup);
 });
