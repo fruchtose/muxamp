1a4b9fe8af7ef177b3b83d772fa09ef6a75ea5f6
Robert Fruchtman <rfruchtose@gmail.com>
2012-12-28 23:41:32 -0700

    Add more UI tests.

---
+++ test/ui.playlist.js
@@ -0,0 +1,25 @@
+var Q 		  = require('q'),
+	Browser   = require('zombie'),
+	testutils = require('../lib/testutils')
+	muxamp 	  = require('../lib/server').getApplication();
+
+describe('Browser playlist interaction', function() {
+	var baseUrl = 'http://localhost:' + 3000 + '/', setup;
+	var browser, window;
+	setup = {
+		before: function() { browser = new Browser(); window = browser.window; },
+		after: function() { browser.close(); }
+	};
+	testutils.server.testWithServer(3000, function() {
+		it('should have the expected UI', function(done) {
+			testutils.expectSuccess(browser.visit(baseUrl), function() {
+				window.$('#controls a').size().should.eql(5); //Back, play, stop, next, shuffle
+			}, done);
+		});
+		it('should be able to navigate to a playlist', function(done) {
+			testutils.expectSuccess(browser.visit(baseUrl + 156), function(browser) {
+				window.Playlist.id.should.eql(156);
+			}, done);
+		});
+	}, setup);
+});
--- test/ui.search.js
+++ test/ui.search.js
@@ -38,7 +38,46 @@ describe('Frontend search', function() {
 				window.$('#search-results tbody tr').size().should.eql(25);
 				window.$('#search-results tbody tr td.title-cell').each(function() {
 					window.$(this).text().length.should.be.above(0);
+				});
+			}, done);
+		});
+	}, setup);
+});
+describe('Adding tracks from search', function() {
+	var baseUrl = 'http://localhost:' + 3000 + '/', setup;
+	var browser, window;
+	setup = {
+		before: function() { browser = new Browser(); window = browser.window; },
+		after: function() { browser.close(); }
+	};
+	testutils.server.testWithServer(3000, function() {
+		it('should give the browser a playlist ID', function(done) {
+			var search = function() {
+				var deferred = Q.defer();
+				browser.visit(baseUrl).then(function() {
+					browser
+						.fill('#search-query', 'MGMT')
+						.pressButton('#search-submit', function() {
+							deferred.resolve(browser);
+						});
+					
+				}, function(error) {
+					throw error;
+				});
+				return deferred.promise;
+			};
+			var addFirstTrack = function(browser) {
+				var deferred = Q.defer();
+				window.Playlist.on('id', function() {
+					deferred.resolve(browser);
 				})
+				window.$('#search-results tr:first-child .search-add-result').click();
+				return deferred.promise;
+			}
+			testutils.expectSuccess(browser.visit(baseUrl).then(search).then(addFirstTrack), function(browser) {
+				window.Playlist.size().should.eql(1);
+				window.Playlist.id.should.be.ok;
+				window.Playlist.id.should.be.above(0);
 			}, done);
 		});
 	}, setup);
