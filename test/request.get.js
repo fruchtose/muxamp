9ca9ac07cbbf12a2fd8d7ff6e890fbcc45e54307
Robert Fruchtman <rfruchtose@gmail.com>
2012-12-28 17:16:06 -0700

    Add more tests.

--- test/request.get.js
+++ test/request.get.js
@@ -7,11 +7,34 @@ describe('GET', function() {
 	var baseUrl = 'http://localhost:' + 3000 + '/';
 	testutils.server.testWithServer(3000, function() {
 		describe('search endpoint', function() {
+			describe('error handling', function() {
+				it('should return an error for no query', function(done) {
+					request({url: baseUrl + 'search/ytv/0/'}, function(err, response, body) {
+						response.statusCode.should.eql(200);
+						JSON.parse(body).should.have.property('error');
+						done();
+					});
+				});
+			});
 			it('should return search results as JSON', function(done) {
 				request({url: baseUrl + 'search/ytv/0/deadmau5'}, function(err, response, body) {
 					response.statusCode.should.eql(200);
 					should.not.exist(err);
 					JSON.parse(body).should.have.length(25);
+					done();
+				});
+			});
+		});
+		describe('playlist endpoint', function() {
+			it('should return search results as JSON', function(done) {
+				request({url: baseUrl + 'playlists/57'}, function(err, response, body) {
+					response.statusCode.should.eql(200);
+					should.not.exist(err);
+					var data = JSON.parse(body);
+					data.should.have.property('id');
+					data['id'].should.eql(57);
+					data.should.have.property('tracks');
+					data['tracks'].should.have.length(5);
 					done();
 				});
 			});
