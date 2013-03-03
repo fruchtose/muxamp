de73262e5c77b8958a28748c4278f526ee6f91b4
Robert Fruchtman <rfruchtose@gmail.com>
2013-03-02 18:02:28 -0700

    Separate config file by different environments.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var muxamp = require('./lib/server');
+var muxamp = require('./lib/server'),
+	config = require('./lib/config');
 
-var app = muxamp.getApplication(), port = muxamp.defaultPort;
+var app = muxamp.getApplication(),
+	environment = config.get('NODE_ENV'),
+	port = config.get(environment + ':port');
 app.listen(port, function() {
 	console.log("Server started, port", port);
 });
