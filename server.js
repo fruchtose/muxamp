4c9a0b6ebc8e5096057cb99ed8cb6017ee535215
Robert Fruchtman <rfruchtose@gmail.com>
2013-03-06 23:05:20 -0700

    More sensible configuration setup. The environment is no longer prepended as the configuration namespace--a very silly way to organize a config hierarchy.

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var muxamp = require('./lib/server'),
 	config = require('./lib/config');
 
 var app = muxamp.getApplication(),
-	environment = config.get('NODE_ENV'),
-	port = config.get(environment + ':port');
+	port = config.get('port');
 app.listen(port, function() {
 	console.log("Server started, port", port);
 });
