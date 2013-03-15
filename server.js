602c58bab7e8dfaa1957bee44e826fec034f39d1
Robert Fruchtman <rfruchtose@gmail.com>
2013-03-14 22:21:14 -0700

    Server starts up with correct port.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var muxamp = require('./lib/server'),
 	config = require('./lib/config');
 
 var app = muxamp.getApplication(),
-	port = config.get('port');
+	port = config.get('muxamp:port');
 app.listen(port, function() {
 	console.log("Server started, port", port);
 });
